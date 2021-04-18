01c263d345724d6174f5364c99dcb74bec31f297
Jonathan Loth <ninoloth@gmail.com>
2021-04-17 22:17:22 -0300

    Atividade composiçao de objetos

---
+++ ComposicaoObjetos/Entities/Worker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ComposicaoObjetos.Entities.Enums;
+using System.Collections.Generic;
+
+namespace ComposicaoObjetos.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>(); // aqui é colocado uma lista porque um Worker pode ter varios Contracts
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department) // aqui o Contracts nao tem construtor porque não é usual colocar lista no construtor, associaçao para muitos nao tem construtor
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract); //Adiciona o contrato na lista Contracts usando o .Add
+        }
+        
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts) // cada contrato que estiver dentro da lista
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month) // parametros para ele só pegar o que for fornecido pelo usuario
+                {
+                    sum += contract.TotalValue(); //o Total Value sempre soma tudo que tiver dentro dos parametros fornecidos
+                }
+            }
+            return sum; // retorna o valor que ta na variavel sum = BaseSalary
+        }
+    }
+
+
+}
---
+++ ComposicaoObjetos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using ComposicaoObjetos.Entities.Enums;
+using ComposicaoObjetos.Entities;
+using System.Globalization;
+
+namespace ComposicaoObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter worder Data");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("level (Junior / MidLevel / Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base Salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName); //instancia o departamento dentro do Department, ele pega o valor que foi digitado e coloca dentro pela variavel deptNaem
+            Worker worker = new Worker(name, level, baseSalary, dept); // aqui tem mais variaveis dentro pq sempre tem que colocar todas que tem la no construtor public Worker
+
+            Console.Write("How many Contracts to this workers: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) // for é apara ele digitar os valores abaixo para o numero de contratos que solicitou
+            {
+                Console.WriteLine($"Enter with the #{n} contract data: ");
+                Console.Write("Date (DD/MM/YYYY):" );
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per Hour:");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (Hours):");
+                int hours = int.Parse(Console.ReadLine());
+
+                HourContract contract = new HourContract(date, valuePerHour, hours); // instanciando as informações no construtor HourContract
+                worker.AddContract(contract); // colocando essas infos para o Worker informado na lista por isso o AddContract
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2)); // aqui ele usa o substring pra pegar o valor da variavel monthAndYera e cortar só o mes
+            int year = int.Parse(monthAndYear.Substring(3));// aqui ele pega o ano
+            Console.WriteLine("Name: " + worker.Name);
+            Console.WriteLine("Department: " +worker.Department.Name);
+            Console.WriteLine("Income for" +monthAndYear +": " + worker.Income(year, month));
+
+        }
+    }
+}
